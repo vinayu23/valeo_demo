ca207db985f8c8106d420bcd32ef60483950d637
vinayu23 <vinaysvpur23@gmail.com>
2026-08-19 18:11:33 +0530

    Create app.cs

---
+++ app.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace HelloArtifactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from .NET 8!");
+ 
